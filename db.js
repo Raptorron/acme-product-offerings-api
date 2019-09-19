4ead54a84b7c1314e3e0ddc1faaadddec84f32da
Matthew Lang <mlang805@yahoo.com>
2019-09-18 19:43:51 -0700

    commiting

---
+++ db.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/offering_db');
+
+
+const syncAndSeed = async ()=>{
+  await conn.sycn({force: true});
+}
+
+// syncAndSeed();
