eaad56319807ef0f7a0708f44264f31f3b884583
XtremePineapple <95henryX@gmail.com>
2019-09-18 19:59:45 -0700

    update

--- db.js
+++ db.js
@@ -1,7 +1,37 @@
 const Sequelize = require('sequelize');
-
+const {UUID, UUIDV4, STRING, DECIMAL} = Sequelize
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/offering_db');
 
+const Product = conn.define('product', {
+  id:{
+    primaryKey: true,
+    type: UUID,
+    defaultValue: UUIDV4
+  },
+  name:{
+    type: STRING,
+    unique:true,
+    allowNull:false
+  },
+  suggestedPrice:{
+    type: DECIMAL
+  }
+})
+const Company = conn.define('comapny', {
+  id:{
+    primaryKey: true,
+    type: UUID,
+    defaultValue: UUIDV4
+  },
+  name:{
+    type: STRING,
+    unique:true,
+    allowNull:false
+  }
+})
+const Offering = conn.define('offering', {
+  price: DECIMAL
+})
 
 const syncAndSeed = async ()=>{
   await conn.sycn({force: true});
