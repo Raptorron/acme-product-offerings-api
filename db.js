067dabe0ef20a886e3d7bfe09d78c95ab4be3df2
Matthew Lang <mlang805@yahoo.com>
2019-09-18 19:44:40 -0700

    forgot to save last save

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/offering_db');
 
 
+
 const syncAndSeed = async ()=>{
   await conn.sycn({force: true});
 }
